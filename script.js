038fa055f547aebe0beb861dc66153cb656e08a0
riishabhraj <105575767+riishabhraj@users.noreply.github.com>
2023-04-06 22:25:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const quizData = [
+  {
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheets",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+  {
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+  },
+];
+
+const quiz = document.getElementById("quiz-header");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+function loadQuiz() {
+  deselectAnswers();
+
+  const data = quizData[currentQuiz];
+
+  questionEl.innerHTML = `${currentQuiz + 1}) ${data.question}`;
+  a_text.innerText = data.a;
+  b_text.innerText = data.b;
+  c_text.innerText = data.c;
+  d_text.innerText = data.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  // check to see the answer
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+      <h4>You answered correctly ${score}/${quizData.length} questions.</h4>
+            `;
+    }
+  }
+});
+
+loadQuiz();
